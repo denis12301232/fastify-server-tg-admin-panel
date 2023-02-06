eba95d5b897cdb44a4fa563076a0ce544876cebd
test12301232 <test12301232@gmail.com>
2023-02-06 12:50:10 +0200

    new

---
+++ public/assets/use-timeout-e651d832.js
@@ -0,0 +1,1 @@
+import{b as u}from"./QBtn-e327acee.js";import{ae as i,D as m,v as s}from"./index-45d0afb7.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(n,r))}}}export{f as u};
