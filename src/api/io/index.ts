cada92da0c2450384a4bed9071b12b65642b283f
test12301232 <test12301232@gmail.com>
2023-03-25 11:49:24 +0200

    socket decomposition

--- src/api/io/index.ts
+++ src/api/io/index.ts
@@ -1,20 +1,28 @@
-import type { Server } from 'socket.io'
+import type { Server, Socket } from 'socket.io'
 import { socketAuthGuard, joinUserChats } from '@/api/io/middlewares'
 import { ACTIONS } from '@/util'
 
 
-export function setupSocketListeners(io: Server) {
-   io.use(socketAuthGuard);
-   io.use(joinUserChats);
-   io.on('connection', async (socket) => {
+export default class IO {
+   private readonly io: Server;
+
+   constructor(io: Server) {
+      this.io = io;
+      this.io.use(socketAuthGuard);
+      this.io.use(joinUserChats);
+   }
+
+   private onConnection(socket: Socket) {
       socket.to(Array.from(socket.rooms)).emit(ACTIONS.UPDATE_STATUS, socket.data.user._id, 'online');
       socket.on(ACTIONS.TYPING, (chat_id: string, user_name: string, user_id: string) => {
-         console.log(chat_id, user_name, user_id);
          socket.to(chat_id).emit('typing', chat_id, user_name, user_id);
       });
-
       socket.on('disconnect', () => {
          socket.to(Array.from(socket.rooms)).emit(ACTIONS.UPDATE_STATUS, socket.data.user._id, 'offline');
-      })
-   });
+      });
+   }
+
+   setListeners() {
+      this.io.on('connection', this.onConnection);
+   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 import factory from '@/factory'
-import { setupSocketListeners } from '@/api/io';
+import IO from '@/api/io';
 
 
 export const app = fastify({ logger: true });
@@ -8,7 +8,7 @@ export const app = fastify({ logger: true });
 async function start() {
    try {
       await factory(app);
-      setupSocketListeners(app.io);
+      new IO(app.io).setListeners();
       await app.listen({ port: process.env.PORT, host: process.env.HOST });
    } catch (e) {
       app.log.error(e);
